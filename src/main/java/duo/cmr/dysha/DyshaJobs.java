1e767959011233c79fd1060b278ac1f7e3373761
Dysil100 <silatsamdylan@gmail.com>
2023-06-04 19:38:13 +0200

    gradle version

--- src/main/java/duo/cmr/dysha/DyshaJobs.java
+++ src/main/java/duo/cmr/dysha/DyshaJobs.java
@@ -1,19 +1,14 @@
 package duo.cmr.dysha;
 
-import duo.cmr.dysha.boundedContexts.dasandere.web.services.subservices.EmailService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.ApplicationEvent;
-import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
-public class DeuxKolosJdbcOauthEmailApplication {
+public class DyshaJobs {
     // TODO: 03.02.22 Fetch all data in an .excel oder .csv file while server ist running
 
     public static void main(String[] args) {
-        SpringApplication.run(DeuxKolosJdbcOauthEmailApplication.class, args);
+        SpringApplication.run(DyshaJobs.class, args);
     }
 
     /*@Autowired
