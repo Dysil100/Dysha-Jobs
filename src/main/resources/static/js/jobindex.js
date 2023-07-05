78047516f49a340b2fe20e3cd3c9cdb778e7eb95
Dysil100 <silatsamdylan@gmail.com>
2023-07-05 02:07:57 +0200

    frontend dysha jobs upgrade

--- src/main/resources/static/js/jobindex.js
+++ src/main/resources/static/js/jobindex.js
@@ -1,5 +1,5 @@
 // Code JavaScript pour récupérer l'image et définir l'icône de la page
-fetch('/files/DyshaJobs_Logo')
+fetch('/dysha_files/DyshaJobs_Logo')
     .then(response => response.blob())
     .then(blob => {
         const url = URL.createObjectURL(blob);
@@ -10,3 +10,16 @@ fetch('/files/DyshaJobs_Logo')
 document.querySelector('.menu-toggle').addEventListener('click', function() {
     document.querySelector('.toolbar ul').classList.toggle('open');
 });
+
+// Récupérer la référence vers l'élément du bouton de menu
+var menuToggle = document.querySelector('.menu-toggle');
+
+// Récupérer la référence vers l'élément de la liste du menu
+var menuList = document.querySelector('nav ul');
+
+// Ajouter un gestionnaire d'événement pour le clic sur le bouton de menu
+menuToggle.addEventListener('click', function() {
+    // Ajouter ou supprimer la classe 'active' pour afficher ou masquer le menu déroulant
+    menuList.classList.toggle('active');
+});
+
