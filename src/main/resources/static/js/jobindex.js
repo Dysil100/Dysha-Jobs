e2a1c1b4f13c1a66fd7a5cc907e25086f8dc6018
Dysil100 <silatsamdylan@gmail.com>
2023-07-05 02:15:49 +0200

    frontend dysha jobs upgrade

--- src/main/resources/static/js/jobindex.js
+++ src/main/resources/static/js/jobindex.js
@@ -1,25 +1,13 @@
 // Code JavaScript pour récupérer l'image et définir l'icône de la page
-fetch('/dysha_files/DyshaJobs_Logo')
-    .then(response => response.blob())
-    .then(blob => {
-        const url = URL.createObjectURL(blob);
-        const favicon = document.getElementById('favicon');
-        favicon.href = url;
-    });
+// Créer une balise <link> pour l'icône
+var iconLink = document.createElement('link');
+iconLink.rel = 'icon';
+iconLink.href = '/files/DyshaJobs_Logo';
+iconLink.type = 'image/*';
+document.head.appendChild(iconLink);
+
 
 document.querySelector('.menu-toggle').addEventListener('click', function() {
     document.querySelector('.toolbar ul').classList.toggle('open');
-});
-
-// Récupérer la référence vers l'élément du bouton de menu
-var menuToggle = document.querySelector('.menu-toggle');
-
-// Récupérer la référence vers l'élément de la liste du menu
-var menuList = document.querySelector('nav ul');
-
-// Ajouter un gestionnaire d'événement pour le clic sur le bouton de menu
-menuToggle.addEventListener('click', function() {
-    // Ajouter ou supprimer la classe 'active' pour afficher ou masquer le menu déroulant
-    menuList.classList.toggle('active');
 });
 
